506085d2f7231fb4b11e0764bd0a3de5e94932c1
Hoang Vu <vviethoang@gmail.com>
2022-06-09 00:02:37 +0700

    chore: use glob instance for lesser init

--- tests/external_services/test_moralis.py
+++ tests/external_services/test_moralis.py
@@ -4,16 +4,16 @@ import pytest
 from src.external_services.moralis import Moralis
 from src.external_services.base import BadRequest, NotFound
 
+client = Moralis(
+    base_url=os.getenv('MORALIS_HOST'),
+    api_key=os.getenv('MORALIS_KEY'),
+)
+
 
 class TestMoralis:
     def test_get_collection_metadata_with_valid_address(self):
         bored_ape_contract_address =\
             '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
-
-        client = Moralis(
-            base_url=os.getenv('MORALIS_HOST'),
-            api_key=os.getenv('MORALIS_KEY'),
-        )
 
         response = client.get_collection_metadata(
             contract_address=bored_ape_contract_address
@@ -30,11 +30,6 @@ class TestMoralis:
         bored_ape_contract_address =\
             '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
 
-        client = Moralis(
-            base_url=os.getenv('MORALIS_HOST'),
-            api_key=os.getenv('MORALIS_KEY'),
-        )
-
         with pytest.raises(BadRequest) as e:
             client.get_collection_metadata(
                 contract_address=bored_ape_contract_address,
@@ -47,11 +42,6 @@ class TestMoralis:
         invalid_contract =\
             '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D2947829svwqq4'
 
-        client = Moralis(
-            base_url=os.getenv('MORALIS_HOST'),
-            api_key=os.getenv('MORALIS_KEY'),
-        )
-
         with pytest.raises(BadRequest) as e:
             client.get_collection_metadata(
                 contract_address=invalid_contract,
@@ -62,11 +52,6 @@ class TestMoralis:
     def test_get_collection_metadata_with_non_exist_contract_address(self):
         invalid_contract =\
             '0xBC4CA0EdA7647A8aB7C2061c2E118A19'
-
-        client = Moralis(
-            base_url=os.getenv('MORALIS_HOST'),
-            api_key=os.getenv('MORALIS_KEY'),
-        )
 
         with pytest.raises(NotFound) as e:
             client.get_collection_metadata(
