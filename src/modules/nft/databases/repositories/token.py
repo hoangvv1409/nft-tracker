2ef728b1ed1f7ab7fa31b4ae2736df28bcb8f741
Hoang Vu <vviethoang@gmail.com>
2022-06-14 21:52:06 +0700

    chore: ignore no stats collection or enough token in collection

--- src/modules/nft/databases/repositories/token.py
+++ src/modules/nft/databases/repositories/token.py
@@ -6,3 +6,10 @@ class TokenRepository(ITokenRepository):
     def __init__(self, session):
         self.session = session
         self.model = TokenSchema
+
+    def count(self, contract_address: str):
+        q = self.session.query(TokenSchema)
+        q = q.filter(
+            TokenSchema.contract_address == contract_address)
+
+        return q.count()
